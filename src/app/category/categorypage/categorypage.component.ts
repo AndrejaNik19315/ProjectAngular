216b9869b6173d35f739a10b22e98a9e95036ffd
Andreja <andreja@scripttic.com>
2019-11-20 13:57:26 +0100

    work on sharing data between components child => parent  (create_post => categorypage)

--- src/app/category/categorypage/categorypage.component.ts
+++ src/app/category/categorypage/categorypage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
+import { CreatePostComponent } from './post/create-post/create-post.component';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -10,7 +11,10 @@ import { AuthService } from 'src/app/core/services/auth.service';
   templateUrl: './categorypage.component.html',
   styleUrls: ['./categorypage.component.css']
 })
-export class CategorypageComponent implements OnInit, OnDestroy {
+export class CategorypageComponent implements OnInit, OnDestroy, AfterViewInit {
+
+  @ViewChild(CreatePostComponent) child;
+
   categories: Array<any>;
   category: any;
   categoryPath: string;
@@ -18,6 +22,12 @@ export class CategorypageComponent implements OnInit, OnDestroy {
   paramsSubscription: Subscription;
   categoryName: any;
   postData = [];
+
+  ngAfterViewInit(): void {
+    if(this.child.post != null){
+      this.postData.unshift(this.child.post);
+    }
+  }
 
   constructor(private route: ActivatedRoute, private titleService: Title, private firebaseService: FirebaseService, public authService: AuthService) {
     this.titleService.setTitle('Divinity - ' + route.snapshot.params['name'].charAt(0).toUpperCase() + route.snapshot.params['name'].slice(1));
@@ -41,26 +51,29 @@ export class CategorypageComponent implements OnInit, OnDestroy {
     this.paramsSubscription.unsubscribe();
   }
 
-  getCategories(){
+  getCategories() {
     this.firebaseService.getCategories().then(result => {
       this.categories = result;
     });
   }
 
-  getCategoryData(categoryPath){
+  getCategoryData(categoryPath) {
     this.firebaseService.getCategory(categoryPath).then(result => {
       this.category = result[0].payload.doc;
     }).then(() => {
       this.firebaseService.getCategoryPosts(this.category.id)
        .then(result => {
-          for(let i = 0; i < result.length; i++) {
-            this.postData.push(result[i].payload.doc.data());
-            this.postData[i].pid = result[i].payload.doc.id;
-            this.firebaseService.getUser(this.postData[i].uid)
-            .then(result => {
-              this.postData[i].displayName = result[0].payload.doc.data().displayName;
-              this.postData[i].userPhotoURL = result[0].payload.doc.data().photoURL;
-            });
+         this.postData  = [];
+         if(result.length > 0){
+            for(let i = 0; i < result.length; i++) {
+              this.postData.push(result[i].payload.doc.data());
+              this.postData[i].pid = result[i].payload.doc.id;
+              this.firebaseService.getUser(this.postData[i].uid)
+              .then(result => {
+                this.postData[i].displayName = result[0].payload.doc.data().displayName;
+                this.postData[i].userPhotoURL = result[0].payload.doc.data().photoURL;
+              });
+            }
           }
         })
        .catch(error => console.log(error));
--- src/app/category/categorypage/post/create-post/create-post.component.ts
+++ src/app/category/categorypage/post/create-post/create-post.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, HostListener } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { FirebaseService } from 'src/app/core/services/firebase.service';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-create-post',
@@ -15,6 +16,8 @@ export class CreatePostComponent implements OnInit {
   photoURL = null;
   errorMessage: string;
   catgeoryId: string;
+  paramsSubscription: Subscription;
+  post: any = null;
   user: any = JSON.parse(localStorage.getItem('user'));
 
   @HostListener('change', ['$event.target.files']) emitFile( event: File ) {
@@ -27,9 +30,11 @@ export class CreatePostComponent implements OnInit {
   constructor(private route: ActivatedRoute, private firebaseService: FirebaseService, private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
-    this.firebaseService.getCategory("category/"+this.route.snapshot.params.name)
+    this.paramsSubscription = this.route.params.subscribe(() => {
+      this.firebaseService.getCategory("category/"+this.route.snapshot.params.name)
     .then(res => this.catgeoryId = res[0].payload.doc.id)
     .catch(error => console.log(error));
+    });
     this.postForm = new FormGroup({
       'postTitle' : new FormControl(null, [Validators.required, Validators.maxLength(32), this.whitespaceValidator]),
       'postImage': new FormControl(this.file, [this.allowedFileType(['png','jpeg','jpg'])]),
@@ -37,6 +42,9 @@ export class CreatePostComponent implements OnInit {
     });
   }
 
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
+  }
   tryPost(values){
     values.uid = this.user.uid;
 
@@ -57,10 +65,14 @@ export class CreatePostComponent implements OnInit {
       }
       this.firebaseService.postCategoryPost(this.catgeoryId, values)
       .then(res => {
+        this.post = {
+
+        }
         let post = document.createElement('div');
         post.innerHTML = "<a href='"+this.route.snapshot.params.name+"/"+res.parent.id+"'><h3 class='card-title'>"+values.postTitle+"</h3></a><p class='card-text mt-2'>"+values.postDescription+"</p><hr/><div class='pl-2 pr-2 text-muted'>Posted just now <a href='user/"+this.user.uid+"'>"+this.user.displayName+"</a></div>";
         document.getElementsByClassName('posts')[0].prepend(post);
         post.className = "card-body";
+
         document.getElementsByTagName("input")[3].click();
       })
       .catch(error => console.log(error));
